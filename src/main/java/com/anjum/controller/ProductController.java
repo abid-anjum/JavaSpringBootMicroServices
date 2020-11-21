2d07f7e6b3666f9935b5e87455e613c1d85f3673
abidhussain <abid786@gmail.com>
2020-11-21 19:21:32 +0300

    add comments

--- src/main/java/com/anjum/controller/ProductController.java
+++ src/main/java/com/anjum/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.anjum.domain.Product;
 import com.anjum.repository.ProductRepository;
-
+//RestController
 @RestController
 public class ProductController {
  
